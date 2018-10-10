5d3415511581b4f1df891d115567980d9450bde8
chamindac <chaminda_chandrasekara@yahoo.com>
2018-10-10 20:54:33 +0800

    Chapter 01 Console App

---
+++ Chapter01/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿#define SHOWERROR
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string welcomeMessage;
+
+#warning a sample custom warning message
+
+//#if SHOWERROR
+//#error SHOWERROR is defined
+//#endif
+
+            Console.WriteLine("Enter your name");
+            string yourName = Console.ReadLine();
+            Console.WriteLine(string.Format("Hello {0}!!! Welcome to Visual Studio.", yourName));
+            Console.ReadLine();
+        }
+    }
+}
