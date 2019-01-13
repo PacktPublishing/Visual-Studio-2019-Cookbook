52b2efba5a3401cf01dbdb7511868889cd734f97
chamindac <chaminda_chandrasekara@yahoo.com>
2019-01-13 23:09:06 +0800

    Region and outline sample code

---
+++ Chapter04/RegionAndOutline/ClassLibrary1/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    /// <summary>
+    /// Class to demonstrate code regions and outlining
+    /// </summary>
+    public class Class1
+    {
+        #region Properties
+        /// <summary>
+        /// Sample property
+        /// </summary>
+        public string Name { get; set; }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Sample public method1
+        /// </summary>
+        public void PublicMethod1()
+        {
+            #region Conditional Check           
+            if (true)
+            {
+                int i;
+                int j=5;
+
+                for (i = 0; i <= j; i++)
+                {
+                }
+            }
+            else
+            {
+
+            }
+            #endregion
+        }
+        #endregion
+
+        #region Private Methods
+        /// <summary>
+        /// Sample private method1
+        /// </summary>
+        public void PrivateMethod1()
+        {
+        }
+
+        public void PrivateMethod2()
+        {
+        }
+        #endregion
+
+    }
+}
