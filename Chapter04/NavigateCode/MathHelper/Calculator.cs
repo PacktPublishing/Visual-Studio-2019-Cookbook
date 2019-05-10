52d819cc66fed3cfad4f2f9dff3356f09eb5c60e
chamindac <chaminda_chandrasekara@yahoo.com>
2019-05-10 15:35:33 +0800

    Recipe Navigate Coe

---
+++ Chapter04/NavigateCode/MathHelper/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathHelper
+{
+    public class Calculator
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Substract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public decimal Divide(int num1, int num2)
+        {
+            if (num2 >0)
+            {
+                return num1 / num2;
+            }
+            else
+            {
+                return 0;
+            }            
+        }
+    }
+}
---
+++ Chapter04/NavigateCode/NavigateCode/TestMath.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavigateCode
+{
+    public class TestMath
+    {
+        public void SimpleMath()
+        {
+            int n1 = 10;
+            int n2 = 2;
+
+            MathHelper.Calculator calc = new MathHelper.Calculator();
+
+            int added = calc.Add(n1, n2);
+            int substracted = calc.Substract(n1, n2);
+            int multiplied = calc.Multiply(n1, n2);
+            decimal divided = calc.Divide(n1, n2);
+        }
+    }
+}
